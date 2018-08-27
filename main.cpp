759cd55d4aa9ed56d8542948c47fe3da948bbc75
Mushud <mushudkofiabuu@gmail.com>
2018-08-27 22:08:19 +0000

    Assignment Two Done, C++ Program to check if an integer is a prime number or not

--- main.cpp
+++ main.cpp
@@ -2,8 +2,33 @@
 
 using namespace std;
 
-int main(){
+bool checkPrime(int number){
+	bool isPrime = true;
+	
+	for(int i=2; i<= number/2; i++){
+		if(number%i == 0){
+			isPrime = false;
+			break;
+		}
+	}
+	
+	return isPrime;
+}
 
-cout << "Hello World" << endl;
-return 0;
+int main(){
+	
+	int number;
+	cout << "Enter an Integer: ";
+	cin >> number;
+	
+	if (number == 1){
+		cout << number << " is not a Prime Number." << endl;
+	}
+	else if (checkPrime(number)){
+		cout << number << " is a Prime Number." << endl;
+	}else if (!checkPrime(number)){
+		cout << number << " is not a Prime Number." << endl;
+	}
+
+	return 0;
 }
